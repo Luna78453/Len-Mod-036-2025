f6fcd9466dd193a468f8415e9c47fc2785031edb
Luna78453 <thesnooppingas@gmail.com>
2025-05-26 15:40:42 -0600

    App finished. Beginning Docker Containment

--- encrypt-decrypt.venv/ecdc/translator.py
+++ encrypt-decrypt.venv/ecdc/translator.py
@@ -6,32 +6,37 @@ from werkzeug.exceptions import abort
 from ecdc.auth import login_required
 from ecdc.db import get_db
 
-from caesar import caesar_encrypt
-
 bp = Blueprint('translator', __name__)
 
-@bp.route('/')
-def index():
-    db = get_db()
-    translations = db.execute(
-        'SELECT p.id, title, txt, translated_txt, created, author_id, username'
-        ' FROM translation p JOIN user u ON p.author_id = u.id'
-        ' ORDER BY created DESC'
-    ).fetchall()
-    return render_template('translator/index.html', translations=translations)
+def shift(_input, shift_amnt):
+    _output = chr(ord(_input) + int(shift_amnt)).encode(encoding="utf-8")
+    return _output
 
-@bp.route('/create', methods=('GET', 'POST'))
+def encryption(_input, shift_amnt):
+    shifted = b''
+
+    for line in _input:
+        for char in line:
+            shifted = shifted + shift(char, shift_amnt)
+    
+    return shifted
+ 
+def caesar_encrypt(_in, shifts):
+    out = encryption(_in, shifts)
+
+    return out
+
+@bp.route('/', methods=('GET', 'POST'))
 @login_required
-def create():
+def index():
     if request.method == 'POST':
-        title = request.form['title']
         txt = request.form['txt'] 
         txtkey = request.form['txtkey']
-        translated_txt = caesar_encrypt(txt, txtkey)
+        translated_txt = str(caesar_encrypt(txt, txtkey), encoding='utf-8')
         error = None
 
-        if not title:
-            error = 'Title is required.'
+        if not txt:
+            error = 'Text is required.'
         
         if not txtkey:
             error = 'key is required.'
@@ -41,18 +46,23 @@ def create():
         else:
             db = get_db()
             db.execute(
-                'INSERT INTO translation (title, txt, txtkey, translated_txt, author_id)'
-                ' VALUES (?, ?, ?)',
-                (title, txt, txtkey, translated_txt, g.user['id'])
+                'INSERT INTO translation (txt, txtkey, translated_txt, author_id)'
+                ' VALUES (?, ?, ?, ?)',
+                (txt, txtkey, translated_txt, g.user['id'])
             )
             db.commit()
             return redirect(url_for('translator.index'))
-
-    return render_template('translator/create.html')
+    db = get_db()
+    translations = db.execute(
+        'SELECT p.id, txt, txtkey, translated_txt, created, author_id, username'
+        ' FROM translation p JOIN user u ON p.author_id = u.id'
+        ' ORDER BY created DESC'
+    ).fetchall()
+    return render_template('translator/index.html', translations=translations)
 
 def get_translation(id, check_author=True):
     translation = get_db().execute(
-        'SELECT p.id, title, txt, txtkey, translated_txt, created, author_id, username'
+        'SELECT p.id, txt, txtkey, translated_txt, created, author_id, username'
         ' FROM translation p JOIN user u ON p.author_id = u.id'
         ' WHERE p.id = ?',
         (id,)
@@ -65,38 +75,6 @@ def get_translation(id, check_author=True):
         abort(403)
 
     return translation
-
-@bp.route('/<int:id>/update', methods=('GET', 'POST'))
-@login_required
-def update(id):
-    translation = get_translation(id)
-
-    if request.method == 'POST':
-        title = request.form['title']
-        txt = request.form['txt']
-        txtkey = request.form['txtkey']
-        translated_txt = caesar_encrypt(txt, txtkey)
-        error = None
-
-        if not title:
-            error = 'Title is required.'
-
-        if not txtkey:
-            error = 'key is required.'
-
-        if error is not None:
-            flash(error)
-        else:
-            db = get_db()
-            db.execute(
-                'UPDATE translation SET title = ?, txt = ?, txtkey = ?, translated_txt = ?'
-                ' WHERE id = ?',
-                (title, txt, txtkey, translated_txt, id)
-            )
-            db.commit()
-            return redirect(url_for('translator.index'))
-
-    return render_template('translator/update.html', translation=translation)
 
 @bp.route('/<int:id>/delete', methods=('POST',))
 @login_required
--- encrypt-decrypt.venv/tests/test_translator.py
+++ encrypt-decrypt.venv/tests/test_translator.py
@@ -10,10 +10,8 @@ def test_index(client, auth):
     auth.login()
     response = client.get('/')
     assert b'Log Out' in response.data
-    assert b'test title' in response.data
     assert b'by test on 2018-01-01' in response.data
     assert b'test\ntxt' in response.data
-    assert b'3' in response.data
     assert b'href="/1/update"' in response.data
 
 @pytest.mark.parametrize('path', (
@@ -52,24 +50,22 @@ def test_exists_required(client, auth, path):
 def test_create(client, auth, app):
     auth.login()
     assert client.get('/create').status_code == 200
-    client.post('/create', data={'title': 'created', 'txt': ''})
+    client.post('/create', data={'txt': 'created'})
 
     with app.app_context():
         db = get_db()
         count = db.execute('SELECT COUNT(id) FROM translation').fetchone()[0]
-        assert count == 2
-
+        assert count == 1
 
 def test_update(client, auth, app):
     auth.login()
     assert client.get('/1/update').status_code == 200
-    client.post('/1/update', data={'title': 'updated', 'txt': ''})
+    client.post('/1/update', data={'txt': 'updated'})
 
     with app.app_context():
         db = get_db()
-        post = db.execute('SELECT * FROM translation WHERE id = 1').fetchone()
-        assert post['title'] == 'updated'
-
+        translation = db.execute('SELECT * FROM translation WHERE id = 1').fetchone()
+        assert translation['txt'] == 'updated'
 
 @pytest.mark.parametrize('path', (
     '/create',
@@ -77,8 +73,8 @@ def test_update(client, auth, app):
 ))
 def test_create_update_validate(client, auth, path):
     auth.login()
-    response = client.post(path, data={'title': '', 'txt': ''})
-    assert b'Title is required.' in response.data
+    response = client.post(path, data={'txt': ''})
+    assert b'Text is required.' in response.data
 
 def test_delete(client, auth, app):
     auth.login()
